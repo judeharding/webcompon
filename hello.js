7c39ff216ff54a98059f51f86c6b6a6efcd5b68e
Jude Harding <jharding@genesisdla.com>
2024-04-11 18:13:55 -0400

    functions vs methods

---
+++ hello.js
@@ -0,0 +1,45 @@
+////Web Components below - class based has an event HANDLER for functions ONLY
+
+// ----------------
+
+// class Test extends HTMLElement {
+// 	connectedCallback() {
+// 		this.innerHTML = `
+//         <div>some text</div>
+//         <button id="btn" onclick="dosomething()">click</button>
+//         `;
+// 	}
+// }
+
+// customElements.define("cust-ele", Test);
+
+// function dosomething() {
+// 	console.log("hello from function");
+// 	document.querySelector("#hellodiv").innerHTML = "hello from function";
+// }
+
+// ----------------
+// ----------------
+////method inside the class uses an event LISTENER for methods
+class Test extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+        <div>some text</div>
+        <button id="btn" >click</button>
+        `;
+	}
+
+	//method
+	dosomething() {
+		console.log("hello from function");
+		document.querySelector("#hellodiv").innerHTML = "hello from function";
+	}
+}
+
+customElements.define("cust-ele", Test);
+
+//event listener
+document.querySelector("#btn").addEventListener("click", function () {
+	new Test().dosomething();
+});
+// ----------------
--- js/index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, Hello from js/index.js!");
-
-import Counter from "./components/Counter.js";
-
-// binding the component
-
-customElements.define("counter-component", Counter);
