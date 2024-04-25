d4c4c5e0e832a80bef62e93aee14f4f7319c3e9f
Jude Harding <jharding@genesisdla.com>
2024-04-24 22:37:34 -0400

    wip coponents

--- indexa.js
+++ indexa.js
@@ -1,30 +1,37 @@
 // REGISTERING MY COMPONENT - my-compa
 class MyComponA extends HTMLElement {
 	connectedCallback() {
-		// RENDER THE HTML IN THE DOM
+		console.log("hello from my-compa LOADED callback");
+		// RENDER THE HTML IN THE DOM of the Index.html file
 		this.innerHTML = `
-        <div id="compAdiv">DIV-A PLAIN DIV W/ SOME TEXT FROM THE INDEX.JS file
-            <div id="div1">
-                <h1>Created Div idofhellodiv to say - Hi Mike in H1</h1>
+        <div id="compAdiv">DIV-A PLAIN DIV W/ SOME TEXT FROM THE INDEXA.JS file
+            <div">
+                <h1>Created Div to say - Hi Mike in H1</h1>
 
-                <button id="btncompa">CLICK TO REMOVE ME</button>
+                <button id="btncompa">CLICK TO REMOVE ME from the WEBCOMPONENTA js file</button>
             </div>
         </div>
         `;
+
+		// cannot use event listener INSIDE of the callback function because of scope?
 		// this.querySelector("btncompa").addEventListener("click", dosomething);
-		console.log("hello from my-compa LOADED");
+
+		// event listener outside of the callback function to
 		registerBtnCompA();
 	}
 }
 
 customElements.define("my-compa", MyComponA);
 
-// // ---
+// // --- END OF THE COMPNENT A
+
+// -- EVENT LISTENER for COMPONENT A Button
 
 function dosomething() {
 	console.log("hello from function dosomethingcomponA ");
 
 	const newDiv = document.createElement("my-compb");
+	newDiv.setAttribute("id", "compBdiv");
 	document.body.appendChild(newDiv);
 
 	document.getElementById("compAdiv").remove();
@@ -33,3 +40,5 @@ function dosomething() {
 function registerBtnCompA() {
 	document.querySelector("#btncompa").addEventListener("click", dosomething);
 }
+
+// ---
--- indexb.js
+++ indexb.js
@@ -2,34 +2,42 @@
 
 class MyComponB extends HTMLElement {
 	connectedCallback() {
+		console.log("hello from my-compb LOADED callback");
 		// RENDER THE HTML IN THE DOM
 		this.innerHTML = `
-        <div id="compBdiv">DIV-B PLAIN DIV W/ SOME TEXT FROM THE INDEX.JS file
-            <div id="div2">
-                <h2>Created Div idofhellodiv to say - Hi Jude in H1</h2>
+        <div id="compBdiv">DIV-B PLAIN DIV W/ SOME TEXT FROM THE INDEXB.JS file
+            <div>
+                <h2>Created Div to say - Hi Jude in H2</h2>
 
-                <button id="btncompb">CLICK TO REMOVE ME</button>
+                <button id="btncompb">CLICK TO REMOVE ME from the WEBCOMPONENTB js file</button>
             </div>
         </div>
+	
         `;
-		console.log("hello from my-compb LOADED");
+
+		// cannot use event listener INSIDE of the callback function because of scope?
+		// this.querySelector("btncompa").addEventListener("click", dosomething);
+
+		// event listener outside of the callback function to
 		registerBtnCompB();
 	}
 }
-
 customElements.define("my-compb", MyComponB);
 
-// // ---
+// // --- END OF THE COMPONENT B ---
+
+// -- EVENT LISTENER for COMPONENT B Button
 
 function dosomethingELSE() {
 	console.log("hello from function dosomethingcomponB ");
 
 	const newDiv = document.createElement("my-compa");
+	newDiv.setAttribute("id", "compAdiv");
 	document.body.appendChild(newDiv);
 
 	document.getElementById("compBdiv").remove();
 }
 
 function registerBtnCompB() {
-	document.querySelector("#btncompb").addEventListener("click", dosomething);
+	document.querySelector("#btncompb").addEventListener("click", dosomethingElse);
 }
