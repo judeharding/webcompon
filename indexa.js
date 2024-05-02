b5e73c6a7e10bb4d27829ad99251863c38ed9b9d
Jude Harding <jharding@genesisdla.com>
2024-05-02 17:32:12 -0400

    components changing via button

--- indexa.js
+++ indexa.js
@@ -1,17 +1,12 @@
 // REGISTERING MY COMPONENT - my-compa
+
 class MyComponA extends HTMLElement {
 	connectedCallback() {
-		console.log("hello from my-compa LOADED callback");
+		console.log("hello from my-compa LOADED callback"); // WORKING
 		// RENDER THE HTML IN THE DOM of the Index.html file
 		this.innerHTML = `
-        <div id="compAdiv">DIV-A PLAIN DIV W/ SOME TEXT FROM THE INDEXA.JS file
-            <div">
-                <h1>Created Div to say - Hi Mike in H1</h1>
-
-                <button id="btncompa">CLICK TO REMOVE ME from the WEBCOMPONENTA js file</button>
-            </div>
-        </div>
-        `;
+                <button id="btncompa">the WEBCOMPONENTA button</button>
+        `; // WORKING
 
 		// cannot use event listener INSIDE of the callback function because of scope?
 		// this.querySelector("btncompa").addEventListener("click", dosomething);
@@ -21,24 +16,38 @@ class MyComponA extends HTMLElement {
 	}
 }
 
-customElements.define("my-compa", MyComponA);
+// this method is calling my component A
+customElements.define("my-compa", MyComponA); // WORKING
 
 // // --- END OF THE COMPNENT A
 
 // -- EVENT LISTENER for COMPONENT A Button
 
 function dosomething() {
-	console.log("hello from function dosomethingcomponA ");
+	console.log("hello from function dosomethingcomponA "); // WORKING
+	// document.querySelector("#myouterwebcomponent").innerHTML = "";
+	document.querySelector("#jude").remove();
+	var parentele = document.querySelector("#myouterwebcomponent");
 
-	const newDiv = document.createElement("my-compb");
-	newDiv.setAttribute("id", "compBdiv");
-	document.body.appendChild(newDiv);
+	const newDiv = document.createElement("my-compb"); // WORKING
+	newDiv.setAttribute("id", "mike"); // WORKING
+	parentele.prepend(newDiv); // WORKING
 
-	document.getElementById("compAdiv").remove();
+	// document.getElementById("compAdiv").remove(); //  WORKING
+	// document.getElementById("my-compa").remove(); // NOT WORKING
+
+	// // Reference to the parent element containing the web component
+	// const parentElement = document.getElementById("myouterwebcomponent");
+
+	// // Reference to the web component itself
+	// const webComponent = document.getElementById("my-compa");
+
+	// // Remove the web component from the parent element
+	// parentElement.removeChild(webComponent);
 }
 
 function registerBtnCompA() {
-	document.querySelector("#btncompa").addEventListener("click", dosomething);
+	document.querySelector("#btncompa").addEventListener("click", dosomething); // WORKING
 }
 
 // ---
--- indexb.js
+++ indexb.js
@@ -5,15 +5,8 @@ class MyComponB extends HTMLElement {
 		console.log("hello from my-compb LOADED callback");
 		// RENDER THE HTML IN THE DOM
 		this.innerHTML = `
-        <div id="compBdiv">DIV-B PLAIN DIV W/ SOME TEXT FROM THE INDEXB.JS file
-            <div>
-                <h2>Created Div to say - Hi Jude in H2</h2>
-
-                <button id="btncompb">CLICK TO REMOVE ME from the WEBCOMPONENTB js file</button>
-            </div>
-        </div>
-	
-        `;
+			<button id="btncompb">the WEBCOMPONENTB button</button> 
+		`;
 
 		// cannot use event listener INSIDE of the callback function because of scope?
 		// this.querySelector("btncompa").addEventListener("click", dosomething);
@@ -31,13 +24,19 @@ customElements.define("my-compb", MyComponB);
 function dosomethingELSE() {
 	console.log("hello from function dosomethingcomponB ");
 
-	const newDiv = document.createElement("my-compa");
-	newDiv.setAttribute("id", "compAdiv");
-	document.body.appendChild(newDiv);
+	// document.querySelector("#myouterwebcomponent").innerHTML = "";
+	document.querySelector("#mike").remove();
 
-	document.getElementById("compBdiv").remove();
+	let myparent = document.querySelector("#myouterwebcomponent");
+
+	const newDiv = document.createElement("my-compa");
+
+	newDiv.setAttribute("id", "jude");
+	myparent.prepend(newDiv);
+
+	// document.querySelector("compBdiv").remove();
 }
 
 function registerBtnCompB() {
-	document.querySelector("#btncompb").addEventListener("click", dosomethingElse);
+	document.querySelector("#btncompb").addEventListener("click", dosomethingELSE);
 }
